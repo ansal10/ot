5c4eda14f29b5ff8bad583cd904b8471a863eddf
ANAS MD <amd@C02KT6QSFFT1.group.on>
2015-11-25 12:01:47 +0530

    Change logback and controller name

---
+++ app/controllers/OtApplication.java
@@ -0,0 +1,32 @@
+package controllers;
+
+import play.Logger;
+import play.mvc.Controller;
+import play.mvc.Result;
+import views.html.ot.ztheme.index;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+
+/**
+ * Created by amd on 11/22/15.
+ */
+public class OtApplication extends Controller {
+
+    public Result index() {
+        String logKey = "Index : ";
+        Logger.debug("DEBUG STARTED SERVING");
+        Logger.info("INFO STARTED SERVICE");
+        Logger.warn("WARN STARTED SERVICE");
+        Logger.error("ERROR STARTED SERVICE");
+
+        try{
+            new Scanner(new File("wqoihdwq"));
+        } catch (FileNotFoundException e) {
+            Logger.error(String.format("Exception %s", e.getMessage()));
+        }
+        return ok(index.render("Hello world"));
+    }
+}
--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -4,7 +4,7 @@
 # --- !Ups
 
 create table option (
-  id                        bigint not null,
+  id                        bigserial not null,
   question_id               bigint,
   option                    varchar(10240),
   correct                   boolean,
@@ -17,14 +17,14 @@ create table permission (
 ;
 
 create table question (
-  id                        bigint not null,
+  id                        bigserial not null,
   question                  varchar(10240),
   constraint uq_question_question unique (question),
   constraint pk_question primary key (id))
 ;
 
 create table result (
-  id                        bigint not null,
+  id                        bigserial not null,
   user_id                   bigint,
   test_id                   bigint,
   response                  varchar(102400),
@@ -34,13 +34,13 @@ create table result (
   correct_answer            bigint,
   wrong_answer              bigint,
   un_attempted              bigint,
-  correct_marks             double,
-  wrong_marks               double,
+  correct_marks             float,
+  wrong_marks               float,
   constraint pk_result primary key (id))
 ;
 
 create table test (
-  id                        bigint not null,
+  id                        bigserial not null,
   name                      varchar(255),
   created_on                timestamp,
   expired_on                timestamp,
@@ -48,22 +48,22 @@ create table test (
   durations                 bigint,
   total_questions           bigint,
   is_active                 boolean,
-  total_marks               double,
+  total_marks               float,
   constraint ck_test_type check (type in (0,1,2,3)),
   constraint pk_test primary key (id))
 ;
 
 create table test_questions (
-  id                        bigint not null,
+  id                        bigserial not null,
   test_id                   bigint,
   question_id               bigint,
-  correct_answer_mark       double,
-  wrong_answer_mark         double,
+  correct_answer_mark       float,
+  wrong_answer_mark         float,
   constraint pk_test_questions primary key (id))
 ;
 
 create table users (
-  id                        bigint not null,
+  id                        bigserial not null,
   username                  varchar(255),
   username_hash             varchar(256),
   password_hash             varchar(256),
@@ -77,62 +77,34 @@ create table users (
   constraint pk_users primary key (id))
 ;
 
-create sequence option_seq;
-
-create sequence question_seq;
-
-create sequence result_seq;
-
-create sequence test_seq;
-
-create sequence test_questions_seq;
-
-create sequence users_seq;
-
-alter table option add constraint fk_option_question_1 foreign key (question_id) references question (id) on delete restrict on update restrict;
+alter table option add constraint fk_option_question_1 foreign key (question_id) references question (id);
 create index ix_option_question_1 on option (question_id);
-alter table permission add constraint fk_permission_user_2 foreign key (user_id) references users (id) on delete restrict on update restrict;
+alter table permission add constraint fk_permission_user_2 foreign key (user_id) references users (id);
 create index ix_permission_user_2 on permission (user_id);
-alter table result add constraint fk_result_user_3 foreign key (user_id) references users (id) on delete restrict on update restrict;
+alter table result add constraint fk_result_user_3 foreign key (user_id) references users (id);
 create index ix_result_user_3 on result (user_id);
-alter table result add constraint fk_result_test_4 foreign key (test_id) references test (id) on delete restrict on update restrict;
+alter table result add constraint fk_result_test_4 foreign key (test_id) references test (id);
 create index ix_result_test_4 on result (test_id);
-alter table test_questions add constraint fk_test_questions_test_5 foreign key (test_id) references test (id) on delete restrict on update restrict;
+alter table test_questions add constraint fk_test_questions_test_5 foreign key (test_id) references test (id);
 create index ix_test_questions_test_5 on test_questions (test_id);
-alter table test_questions add constraint fk_test_questions_question_6 foreign key (question_id) references question (id) on delete restrict on update restrict;
+alter table test_questions add constraint fk_test_questions_question_6 foreign key (question_id) references question (id);
 create index ix_test_questions_question_6 on test_questions (question_id);
 
 
 
 # --- !Downs
 
-SET REFERENTIAL_INTEGRITY FALSE;
+drop table if exists option cascade;
 
-drop table if exists option;
+drop table if exists permission cascade;
 
-drop table if exists permission;
+drop table if exists question cascade;
 
-drop table if exists question;
+drop table if exists result cascade;
 
-drop table if exists result;
+drop table if exists test cascade;
 
-drop table if exists test;
+drop table if exists test_questions cascade;
 
-drop table if exists test_questions;
-
-drop table if exists users;
-
-SET REFERENTIAL_INTEGRITY TRUE;
-
-drop sequence if exists option_seq;
-
-drop sequence if exists question_seq;
-
-drop sequence if exists result_seq;
-
-drop sequence if exists test_seq;
-
-drop sequence if exists test_questions_seq;
-
-drop sequence if exists users_seq;
+drop table if exists users cascade;
 
